f4fee93af86adba29b02b93fa14660d72d581370
krjumde <krjumde@gmail.com>
2021-11-28 15:18:26 +0530

    sortedEmployeeUsingSalary

--- src/com/SortedEmployeeDeptUsingSalary.java
+++ src/com/SortedEmployeeDeptUsingSalary.java
@@ -3,6 +3,7 @@ package com;
 import com.pojo.Employee;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SortedEmployeeDeptUsingSalary {
     public void sortedEmployeeUsingSalary(){
@@ -45,6 +46,48 @@ public class SortedEmployeeDeptUsingSalary {
         for (Map.Entry<String, ArrayList<Employee>> s1 : set) {
             List<Employee> elist = s1.getValue();
             for (Employee e1 : elist) {
+                System.out.println();
+                System.out.println(e1.getId() + ":" + e1.getDept() + ":" + e1.getSalary());
+                break;
+            }
+        }
+
+    }
+
+    //https://www.onlinetutorialspoint.com/java8/java-8-groupingby-example.html
+
+    public void sortEmployeeUsingJava8() {
+
+        List<com.sorting.Employee> employeeList = new ArrayList<com.sorting.Employee>();
+        employeeList.add(new com.sorting.Employee(1, "A", 10000));
+        employeeList.add(new com.sorting.Employee(2, "A", 20000));
+        employeeList.add(new com.sorting.Employee(3, "A", 15000));
+        employeeList.add(new com.sorting.Employee(10, "A", 17000));
+
+        employeeList.add(new com.sorting.Employee(4, "B", 30000));
+        employeeList.add(new com.sorting.Employee(5, "B", 50000));
+        employeeList.add(new com.sorting.Employee(6, "B", 40000));
+        employeeList.add(new com.sorting.Employee(11, "B", 37000));
+
+        employeeList.add(new com.sorting.Employee(7, "C", 70000));
+        employeeList.add(new com.sorting.Employee(8, "C", 90000));
+        employeeList.add(new com.sorting.Employee(9, "C", 80000));
+        employeeList.add(new com.sorting.Employee(12, "C", 77000));
+
+        Map<String, List<com.sorting.Employee>> byDept = employeeList.stream().collect(
+                Collectors.groupingBy(x -> x.getDept()));
+
+        Set<Map.Entry<String, List<com.sorting.Employee>>> set = byDept.entrySet().stream().map(m1 -> {
+                    m1.setValue(m1.getValue().stream().sorted(Comparator.comparingInt(com.sorting.Employee::getSalary).reversed()).collect(Collectors.toList()));
+                    return m1;
+                }
+        ).collect(Collectors.toSet());
+
+
+        for (Map.Entry<String, List<com.sorting.Employee>> s1 : set) {
+            List<com.sorting.Employee> elist = s1.getValue();
+            for (com.sorting.Employee e1 : elist) {
+
                 System.out.println();
                 System.out.println(e1.getId() + ":" + e1.getDept() + ":" + e1.getSalary());
                 break;
